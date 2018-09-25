15990afa006452c0320d2592ac3fd71dacae56b0
Manish Mangyani <38870393+manishnm@users.noreply.github.com>
2018-09-26 04:36:18 +0530

    Update fbcallback.php

--- ab/fbcallback.php
+++ ab/fbcallback.php
@@ -30,6 +30,6 @@
 //	echo "<a href='https://rtdownloader.000webhostapp.com/albums.php'>goto albums</a>";
 	//var_dump($userData);
 	$_SESSION['access_token'] = (string) $accessToken;
-	header('Location:https://abhishekrt.xyz/albums.php');
+	header('Location: albums.php');
 	//exit();
 ?>
